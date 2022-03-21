88f953ca30a747feda2d690118791076a4862ef5
Rafael de Jesus Saraiva <rafajs7@gmail.com>
2022-03-21 03:00:15 +0000

    Updated Content of Help Pages

--- pages/help/terms-of-service.tsx
+++ pages/help/terms-of-service.tsx
@@ -1,3 +1,138 @@
-export default function PrivacyPolicy() {
-  return <h1>Terms of service Page</h1>;
+import { Typography } from "@mui/material";
+import Link from "next/link";
+import type { ReactElement } from "react";
+import Layout from "../../components/layout-help";
+
+export default function Help_TermsOfService() {
+  return (
+    <>
+      <Typography component="h2" variant="h4">
+        1. Termos
+      </Typography>
+      <p>
+        Ao acessar ao site{" "}
+        <Link href="https://provas.rafaeljesusaraiva.pt">
+          Provas Fotografia
+        </Link>
+        , concorda em cumprir estes termos de serviço, todas as leis e
+        regulamentos aplicáveis ​​e concorda que é responsável pelo cumprimento
+        de todas as leis locais aplicáveis. Se você não concordar com algum
+        desses termos, está proibido de usar ou acessar este site. Os materiais
+        contidos neste site são protegidos pelas leis de direitos autorais e
+        marcas comerciais aplicáveis.
+      </p>
+      <Typography component="h2" variant="h4">
+        2. Uso de Licença
+      </Typography>
+      <p>
+        É concedida permissão para baixar temporariamente uma cópia dos
+        materiais (informações ou software) no site Provas Fotografia , apenas
+        para visualização transitória pessoal e não comercial. Esta é a
+        concessão de uma licença, não uma transferência de título e, sob esta
+        licença, você não pode:{" "}
+      </p>
+      <ol>
+        {" "}
+        <li>modificar ou copiar os materiais; </li>{" "}
+        <li>
+          usar os materiais para qualquer finalidade comercial ou para exibição
+          pública (comercial ou não comercial);{" "}
+        </li>{" "}
+        <li>
+          tentar descompilar ou fazer engenharia reversa de qualquer software
+          contido no site Provas Fotografia;{" "}
+        </li>{" "}
+        <li>
+          remover quaisquer direitos autorais ou outras notações de propriedade
+          dos materiais; ou{" "}
+        </li>{" "}
+        <li>
+          transferir os materiais para outra pessoa ou &apos;espelhe&apos; os
+          materiais em qualquer outro servidor.
+        </li>{" "}
+      </ol>
+      <p>
+        Esta licença será automaticamente rescindida se você violar alguma
+        dessas restrições e poderá ser rescindida por Provas Fotografia a
+        qualquer momento. Ao encerrar a visualização desses materiais ou após o
+        término desta licença, você deve apagar todos os materiais baixados em
+        sua posse, seja em formato eletrónico ou impresso.
+      </p>
+      <Typography component="h2" variant="h4">
+        3. Isenção de responsabilidade
+      </Typography>
+      <ol>
+        {" "}
+        <li>
+          Os materiais no site da Provas Fotografia são fornecidos &apos;como
+          estão&apos;. Provas Fotografia não oferece garantias, expressas ou
+          implícitas, e, por este meio, isenta e nega todas as outras garantias,
+          incluindo, sem limitação, garantias implícitas ou condições de
+          comercialização, adequação a um fim específico ou não violação de
+          propriedade intelectual ou outra violação de direitos.{" "}
+        </li>{" "}
+        <li>
+          Além disso, o Provas Fotografia não garante ou faz qualquer
+          representação relativa à precisão, aos resultados prováveis ​​ou à
+          confiabilidade do uso dos materiais em seu site ou de outra forma
+          relacionado a esses materiais ou em sites vinculados a este site.
+        </li>{" "}
+      </ol>
+      <Typography component="h2" variant="h4">
+        4. Limitações
+      </Typography>
+      <p>
+        Em nenhum caso o Provas Fotografia ou seus fornecedores serão
+        responsáveis ​​por quaisquer danos (incluindo, sem limitação, danos por
+        perda de dados ou lucro ou devido a interrupção dos negócios)
+        decorrentes do uso ou da incapacidade de usar os materiais em Provas
+        Fotografia, mesmo que Provas Fotografia ou um representante autorizado
+        da Provas Fotografia tenha sido notificado oralmente ou por escrito da
+        possibilidade de tais danos. Como algumas jurisdições não permitem
+        limitações em garantias implícitas, ou limitações de responsabilidade
+        por danos conseqüentes ou incidentais, essas limitações podem não se
+        aplicar a você.
+      </p>
+      <Typography component="h2" variant="h4">
+        5. Precisão dos materiais
+      </Typography>
+      <p>
+        Os materiais exibidos no site da Provas Fotografia podem incluir erros
+        técnicos, tipográficos ou fotográficos. Provas Fotografia não garante
+        que qualquer material em seu site seja preciso, completo ou atual.
+        Provas Fotografia pode fazer alterações nos materiais contidos em seu
+        site a qualquer momento, sem aviso prévio. No entanto, Provas Fotografia
+        não se compromete a atualizar os materiais.
+      </p>
+      <Typography component="h2" variant="h4">
+        6. Links
+      </Typography>
+      <p>
+        O Provas Fotografia não analisou todos os sites vinculados ao seu site e
+        não é responsável pelo conteúdo de nenhum site vinculado. A inclusão de
+        qualquer link não implica endosso por Provas Fotografia do site. O uso
+        de qualquer site vinculado é por conta e risco do usuário.
+      </p>{" "}
+      <Typography component="h3" variant="h5">
+        Modificações
+      </Typography>{" "}
+      <p>
+        O Provas Fotografia pode revisar estes termos de serviço do site a
+        qualquer momento, sem aviso prévio. Ao usar este site, você concorda em
+        ficar vinculado à versão atual desses termos de serviço.
+      </p>{" "}
+      <Typography component="h3" variant="h5">
+        Lei aplicável
+      </Typography>{" "}
+      <p>
+        Estes termos e condições são regidos e interpretados de acordo com as
+        leis do Provas Fotografia e você se submete irrevogavelmente à
+        jurisdição exclusiva dos tribunais naquele estado ou localidade.
+      </p>
+    </>
+  );
 }
+
+Help_TermsOfService.getLayout = function getLayout(page: ReactElement) {
+  return <Layout title="Termos de Serviço">{page}</Layout>;
+};
--- pages/help/want-to-delete-my-data.tsx
+++ pages/help/want-to-delete-my-data.tsx
@@ -1,3 +1,157 @@
-export default function PrivacyPolicy() {
-  return <h1>Want to delete my data Page</h1>;
+import { Typography } from "@mui/material";
+import Link from "next/link";
+import type { ReactElement } from "react";
+import Layout from "../../components/layout-help";
+
+export default function Help_DeleteData() {
+  return (
+    <>
+      <Typography component="h2" variant="h4">
+        Quero Que Apaguem Os Meus Dados
+      </Typography>
+      <p>
+        Ao abrigo do Regulamento Geral de Protecção de Dados (RGPD), tem o
+        direito de ver os seus dados apagados e removidos dos nossos sistemas
+        (este é por vezes chamado o &apos;direito a ser esquecido&apos;).
+      </p>{" "}
+      <p>
+        Se desejar fazer um Pedido de Eliminação de Dados, envie um email para
+        webdev@rafaeljesusaraiva.pt com o assunto &apos;GDPR - Apagar Dados -
+        &apos;, seguido do seu nome completo (mais informações sobre o que
+        enviar nos parágrafos seguintes).
+      </p>{" "}
+      <p>
+        Processaremos o seu pedido em conformidade com o Regulamento Geral de
+        Protecção de Dados -
+      </p>{" "}
+      <p>
+        <Link href="https://gdprinfo.eu/pt-pt">Saiba mais sobre a RGPD</Link>
+      </p>{" "}
+      <p>
+        <Link href="/help/privacy-policy">
+          Aceder à nossa Declaração de Privacidade
+        </Link>
+      </p>{" "}
+      <p>
+        Tenha em atenção que, apesar do seu pedido de eliminação dos seus dados,
+        existem razões específicas pelas quais podemos recusar o seu pedido e os
+        seus dados continuarão a ser processados. Estas razões são apresentadas
+        a seguir.
+      </p>{" "}
+      <p>
+        Tem o direito de ver os seus dados apagados nas seguintes circunstâncias
+        -
+      </p>{" "}
+      <ul>
+        {" "}
+        <li>
+          {" "}
+          Quando os dados pessoais já não são necessários em relação à
+          finalidade para a qual a Provas Fotografia originalmente os recolheu
+          ou processou
+          <br />
+          <br />{" "}
+        </li>{" "}
+        <li>
+          {" "}
+          Quando tiver retirado o seu consentimento para a continuação do
+          processamento dos dados e não houver outra base legal para a Provas
+          Fotografia continuar a processar os dados
+          <br />
+          <br />{" "}
+        </li>{" "}
+        <li>
+          {" "}
+          Onde se opõe ao processamento. Neste caso, a Provas Fotografia
+          determinará se tem um interesse legítimo primordial em continuar o
+          processamento
+          <br />
+          <br />{" "}
+        </li>{" "}
+        <li>
+          {" "}
+          Os dados pessoais foram tratados de forma ilegal
+          <br />
+          <br />{" "}
+        </li>{" "}
+        <li>
+          {" "}
+          Os dados pessoais têm de ser apagados devido a uma obrigação legal
+          <br />
+          <br />{" "}
+        </li>{" "}
+      </ul>{" "}
+      <p>
+        Ao receber o seu pedido, a Provas Fotografia responderá no prazo de um
+        mês civil a contar do dia seguinte ao da sua recepção.
+      </p>{" "}
+      <p>
+        Apagaremos os dados, a menos que tenhamos uma razão imperiosa para
+        continuar a processá-los ou que sejam processados por uma das seguintes
+        razões -
+      </p>{" "}
+      <ul>
+        {" "}
+        <li>
+          {" "}
+          exercer os direitos de liberdade de expressão e de informação <br />
+          <br />{" "}
+        </li>{" "}
+        <li>
+          {" "}
+          para que o Website cumpra um requisito legal
+          <br />
+          <br />{" "}
+        </li>{" "}
+        <li>
+          {" "}
+          o desempenho de uma tarefa realizada no interesse público ou no
+          exercício da autoridade pública
+          <br />
+          <br />{" "}
+        </li>{" "}
+        <li>
+          {" "}
+          para fins de saúde pública no interesse público
+          <br />
+          <br />{" "}
+        </li>{" "}
+        <li>
+          {" "}
+          fins de arquivo no interesse público, investigação histórica
+          científica ou estatística ou
+          <br />
+          <br />{" "}
+        </li>{" "}
+        <li>
+          {" "}
+          a defesa das acções judiciais
+          <br />
+          <br />{" "}
+        </li>{" "}
+      </ul>{" "}
+      <p>
+        Se uma das circunstâncias acima se aplicar, o seu pedido de eliminação
+        será recusado e nós escrever-lhe-emos indicando o motivo da recusa.
+      </p>{" "}
+      <p>
+        Uma vez escolhidas as razões, enumerar e enviar para
+        webdev@rafaeljesusaraiva.pt com as orientações indicadas inicialmente.
+      </p>{" "}
+      <h3>Mais informações</h3>{" "}
+      <p>
+        Esperemos que esteja esclarecido e, como mencionado anteriormente, se
+        houver algo que você não tem certeza, entre em contacto com os
+        administradores do website/serviço para mais informações.
+      </p>{" "}
+      <p>
+        Esta política é efetiva a partir de <strong>Março</strong>/
+        <strong>2022</strong>.
+      </p>
+    </>
+  );
 }
+
+Help_DeleteData.getLayout = function getLayout(page: ReactElement) {
+  return <Layout title="Quero Que Apaguem Os Meus Dados">{page}</Layout>;
+};
