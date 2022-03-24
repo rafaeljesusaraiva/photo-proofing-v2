57d35eb21af279abf5fb6590eeb25bb17f8a399d
Rafael de Jesus Saraiva <rafajs7@gmail.com>
2022-03-24 00:31:39 +0000

    Create layout_noSSR.tsx

---
+++ components/layout_noSSR.tsx
@@ -0,0 +1,40 @@
+import { ReactNode, useEffect, useLayoutEffect, useState } from "react";
+import PropTypes from "prop-types";
+
+const useEnhancedEffect =
+  typeof window !== "undefined" && process.env.NODE_ENV !== "test"
+    ? useLayoutEffect
+    : useEffect;
+
+type NoSSRType = {
+  children: ReactNode;
+  defer: any;
+  fallback: any;
+};
+
+const NoSSR = ({ children, defer, fallback }: NoSSRType) => {
+  const [isMounted, setMountedState] = useState(false);
+
+  useEnhancedEffect(() => {
+    if (!defer) setMountedState(true);
+  }, [defer]);
+
+  useEffect(() => {
+    if (defer) setMountedState(true);
+  }, [defer]);
+
+  return isMounted ? children : fallback;
+};
+
+NoSSR.propTypes = {
+  children: PropTypes.node.isRequired,
+  defer: PropTypes.bool,
+  fallback: PropTypes.node,
+};
+
+NoSSR.defaultProps = {
+  defer: false,
+  fallback: null,
+};
+
+export default NoSSR;
